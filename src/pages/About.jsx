b6bc79d834d0ae1b2e02813547b31ae49345631c
SilvinaAlvarez2811 <silvinaalvarez2811@gmail.com>
2025-06-17 17:08:07 -0300

    Se agrega pagina para Login y rutas

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div>Sobre Nosotros</div>;
+}
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contact() {
+  return <div>Contacto</div>;
+}
+
+export default Contact;
